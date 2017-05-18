d0358d8a09fd9785b4ca9d54a582cda94b7e7ce6
Enes Faruk Meniz <EnesFaruk.Meniz@telenity.com>
2017-05-18 10:34:24 +0300

    minor update so refresh wont be need 99% of the time

--- content-script.js
+++ content-script.js
@@ -1,1 +1,7 @@
 chrome.runtime.sendMessage({loadURL: true});
+
+$("a").each(function (i, el) {
+    if ($(el).attr("href")) {
+        $(el).attr("href", $(el).attr("href").replace(/(\?ref.*)/, ''))
+    }
+})
